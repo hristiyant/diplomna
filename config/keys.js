1a936a9c333e8394169dacb2016fc570bbbc40bf
Todorov <hristiyan.todorov@dxc.com>
2021-05-27 16:36:33 +0300

    Setting up API routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://user_1:user_1@cluster0.6ok47.mongodb.net/diplomnaDB?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://user_1:user_1@cluster0.6ok47.mongodb.net/diplomnaDB?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 }
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
 const app = express();
+
 // Bodyparser middleware
 app.use(
-  express.urlencoded({
+  bodyParser.urlencoded({
     extended: false
   })
 );
-app.use(express.json());
+app.use(bodyParser.json());
+
 // DB Config
 const db = require("./config/keys").mongoURI;
+
 // Connect to MongoDB
 mongoose
   .connect(
@@ -18,5 +26,16 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require("./config/passport")(passport);
+
+//Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if I choose to deploy the app there
+
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
