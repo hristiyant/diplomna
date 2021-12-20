19cbb832380744b30efcb49b68275515abc2d480
Todorov <hristiyan.todorov@dxc.com>
2021-12-20 17:06:21 +0200

    UI tweaks. Add custom scrollbar.

--- client/src/components/events/RecordList.js
+++ client/src/components/events/RecordList.js
@@ -5,6 +5,7 @@ import Loader from "react-loader-spinner";
 
 import { confirmAlert } from 'react-confirm-alert';
 import 'react-confirm-alert/src/react-confirm-alert.css';
+import { Scrollbars } from "react-custom-scrollbars";
 
 import { getEvents, subscribeToEvent, deleteEvent } from "../../actions/eventActions";
 import { getDisplayDate } from "../../utils/DateUtils";
@@ -97,32 +98,38 @@ const RecordList = (props) => {
       <div className="friend-requests" >
         <input className="search-box" onInput={filterCards} placeholder="Search..." />
         <div className="cards-grid-requests">
-          {data.map((event, index) => (
-            <div key={index} className="card-requests">
-              <div className="card-header">
-                {event.name}
-                {/* <img className="profileUserImage" src="logo.png" alt="" /> */}
+          <Scrollbars className="my-scrollbar" style={{ width: "100%", height: "100%" }}
+            renderTrackVertical={props => <div {...props} className="track-vertical" />}
+            renderThumbVertical={props => <div {...props} className="thumb-vertical" />}
+            renderView={props => <div {...props} className="view" />}
+          >
+            {data.map((event, index) => (
+              <div key={index} className="card-requests">
+                <div className="card-header">
+                  {event.name}
+                  {/* <img className="profileUserImage" src="logo.png" alt="" /> */}
+                </div>
+                <div className="card-body">
+                  <span>Date: {getDisplayDate(event.date)}</span>
+                  <div>Created by: {event.createdByName}</div>
+                  <div>Participants: {event.participants.length} / {event.quota}</div>
+                  {/* <div className="card__image"><img src={userData.picture.medium}/></div> */}
+                </div>
+                <div className="card-footer">
+                  {showSubscribeButton(event) && <button className="btn-accept" onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    onSubscribeClick(event);
+                  }}>SUBSCRIBE</button>}
+                  {showCancelEventButton(event) && <button className="btn-accept btn-outline" onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    onDeleteClick(event)
+                  }}>CANCEL EVENT</button>}
+                </div>
               </div>
-              <div className="card-body">
-                <span>Date: {getDisplayDate(event.date)}</span>
-                <div>Created by: {event.createdByName}</div>
-                <div>Participants: {event.participants.length} / {event.quota}</div>
-                {/* <div className="card__image"><img src={userData.picture.medium}/></div> */}
-              </div>
-              <div className="card-footer">
-                {showSubscribeButton(event) && <button className="btn-accept" onClick={(e) => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  onSubscribeClick(event);
-                }}>SUBSCRIBE</button>}
-                {showCancelEventButton(event) && <button className="btn-accept btn-outline" onClick={(e) => {
-                  e.stopPropagation();
-                  e.preventDefault();
-                  onDeleteClick(event)
-                }}>CANCEL EVENT</button>}
-              </div>
-            </div>
-          ))}
+            ))}
+          </Scrollbars>
         </div>
       </div >
     );
